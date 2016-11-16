07fe0d465ed51148857ed5c191c8f88d16c29156
[panlin] <[pmumuv5@163.com]>
2016-11-16 23:40:50 +0800

    menu list preview

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,13 +1,51 @@
 $(function () {
 
+    $('.modal').modal();  //初始化弹窗
+
+
+
+
     $(".preview-btn").on("click",function () {
+
+        var menu_list = $(".menu-list tbody");
         var menu_text =  $("#menu-text").val();
-        var menu_arr  = text2arr(menu_text);
+
+        console.log(menu_list.length);
+        if (menu_text == ""){
+            Materialize.toast('请先输入菜单', 2000);
+            return false;
+        }
+        createMenu(menu_list);
+
+        $('.modal').modal("open");
+
 
 
 
 
     });
+
+
+    //生成菜单列表
+    function createMenu(el) {
+        var menu_arr = GetMenuArr();
+
+        el.html("");
+
+        console.log(el.length);
+
+        for (var i = 0;i < menu_arr.length;i++){
+            el.append("<tr><td>"+ menu_arr[i] +"</td></tr>")
+        }
+        console.log(menu_arr);
+    }
+
+
+    //序列化菜单数组
+    function GetMenuArr() {
+        var menu_text =  $("#menu-text").val();
+        return text2arr(menu_text);
+    }
 
 
     //获取菜单数组
